187c85c4bdb410bad1a211f8fa359cbba009c678
Nidhish Chauhan <rch246chauhan@gmail.com>
2022-12-03 23:28:57 +0530

    NavBar work 50% done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import Menu from './components/Menu'
 import NavBar from './components/NavBar'
+import styles from "./App.module.scss"
 
 const App = () => {
   return (
-    <div className="flex ">
-
-      <Menu>
-        <div>
-          <NavBar></NavBar>
-          <div>
-Videos
-          </div>
-        </div>
-      </Menu>
+    <div className={styles.container}>
+      <Menu></Menu>
+      <div className={styles.main}>
+        <NavBar></NavBar>
+        <div className={styles.wrapper}>Videos</div>
+      </div>
     </div>
   )
 }
--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import styles from "./Menu.module.scss"
+import YoutubeLogo from '../images/youtube.png'
+
+import {RiHome5Fill,RiHome5Line} from 'react-icons/ri'
 
 const Menu = () => {
   return (
-    <div className="flex-1">Menu</div>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.logo}>
+          <img src={YoutubeLogo} alt="" />
+          MeTube2
+        </div>
+        <div className={styles.item}>
+        <RiHome5Fill/>
+          HOme
+        </div>
+      </div>
+    </div>
   )
 }
 
