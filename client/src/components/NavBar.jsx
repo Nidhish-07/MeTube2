1ade17b25e0a1911c6979737083a44e87c4a11d2
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-01-05 19:08:12 +0530

    Navbar added

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { AiOutlineSearch } from "react-icons/ai"
 
 const NavBar = () => {
     return (
-        <div>NavBar</div>
+        <div className='sticky top-0 bg-white h-14 '>
+            <div className='flex justify-end relative items-center h-full py-0 px-5'>
+                <div className='absolute left-0 right-0 mx-auto flex w-1/2 items-center justify-between p-1 border-[1px] border-solid border-gray-400'>
+                    <input placeholder='Search' className='border-none bg-transparent'></input>
+                    <AiOutlineSearch size={24} fontWeight={"bold"}></AiOutlineSearch>
+                </div>
+                <button className='py-1 px-4 bg-transparent border-blue-400 text-blue-400 border-[1px] font-medium cursor-pointer'>Sign In</button>
+            </div>
+
+        </div>
     )
 }
 
